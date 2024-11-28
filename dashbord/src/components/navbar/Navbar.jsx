5bb3fc9ff0658bd3ee58d1b83a5044e8fd2374ff
mnosumon <mdnuruddin378350@gmail.com>
2024-11-28 22:48:39 +0600

    dashbord design

---
+++ dashbord/src/components/navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav_wrapper">
+      <div className="left">
+        <ul className="items">
+          <li>Home </li>
+          <li>About </li>
+          <li>Service </li>
+          <li>Resum </li>
+          <li>Portfoli </li>
+          <li>Testimon</li>
+          <li>Blog </li>
+        </ul>
+      </div>
+      <div className="right">sdfasd</div>
+    </div>
+  );
+};
+
+export default Navbar;
