4652f8b420d07b44ffa5cab442620b3428e0afee
mnosumon <mdnuruddin378350@gmail.com>
2024-11-29 23:59:27 +0600

    data fetch done from backend

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./navbar.css";
 import Logo from "../../assets/image/logo.png";
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [list, setList] = useState("");
+  console.log(list);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get("http://localhost:8000/navitem");
+      setList(data.data.menuItem.split(","));
+    };
+    fetchData();
+  }, []);
   return (
     <section className="navSection">
       <nav className="navbar container">
@@ -12,27 +25,9 @@ const Navbar = () => {
           </a>
         </div>
         <ul className="nav-links">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Resume</a>
-          </li>
-          <li>
-            <a href="#">Portfolio</a>
-          </li>
-          <li>
-            <a href="#">Testimonial</a>
-          </li>
-          <li>
-            <a href="#">Blog</a>
-          </li>
+          {list.map((item) => (
+            <li>{item}</li>
+          ))}
         </ul>
         <div className="contact-button">
           <a href="#">Contact Us</a>
