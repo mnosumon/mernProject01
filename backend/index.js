29b73fed6fcafe529245e28b1629aea5f99df321
mnosumon <mdnuruddin378350@gmail.com>
2024-11-29 19:26:33 +0600

    database connect

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 const cors = require("cors");
+
+const { Schema } = mongoose;
+
+mongoose
+  .connect(
+    "mongodb+srv://mdnuruddin378350:zrZ3PQ5VQGu1uGwR@cluster0.91n45.mongodb.net/nav?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Connected!"));
 
 app.use(cors());
 app.use(express.json());
