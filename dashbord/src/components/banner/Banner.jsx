9fe000b9be02ab2537c24fe7c929888ba1563ea8
mnosumon <mdnuruddin378350@gmail.com>
2024-12-04 23:58:06 +0600

    banner handle change

--- dashbord/src/components/banner/Banner.jsx
+++ dashbord/src/components/banner/Banner.jsx
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./banner.css";
 
 const Banner = () => {
+  const [subHeading, setSubHeading] = useState("");
+  const [heading, setHeading] = useState("");
+  const [paregraph, setParegraph] = useState("");
+  const [buttonText, setButtonText] = useState("");
+  const [buttonShow, setButtonShow] = useState(false);
+  console.log(buttonShow);
+
   const handleSubmit = () => {
     console.log("hello");
+  };
+  const handleSubHeading = (e) => {
+    setSubHeading(e.target.value);
+  };
+  const handleHeading = (e) => {
+    setHeading(e.target.value);
+  };
+  const handleParegrap = (e) => {
+    setParegraph(e.target.value);
+  };
+  const handleButtonText = (e) => {
+    setButtonText(e.target.value);
+  };
+  const handleButtonShow = (e) => {
+    setButtonShow(e.target.checked);
   };
 
   return (
     <div class="input-container">
-      <input type="text" class="input-field" placeholder="Subheading" />
-      <input type="text" class="input-field" placeholder="Heading" />
-      <input type="text" class="input-field" placeholder="Paragraph" />
-      <label class="checkbox-wrapper">
+      <input
+        onChange={handleSubHeading}
+        type="text"
+        class="input-field"
+        placeholder="Subheading"
+      />
+      <input
+        onChange={handleHeading}
+        type="text"
+        class="input-field"
+        placeholder="Heading"
+      />
+      <input
+        onChange={handleParegrap}
+        type="text"
+        class="input-field"
+        placeholder="Paragraph"
+      />
+      <input
+        onChange={handleButtonText}
+        type="text"
+        class="input-field"
+        placeholder="button text"
+      />
+      <label onChange={handleButtonShow} class="checkbox-wrapper">
         <input type="checkbox" class="checkbox" />
         <span>Agree to terms</span>
       </label>
